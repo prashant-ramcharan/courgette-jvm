14f97d9b547cb822430343c9ba4b6a2af2eb1b7e
Prashant Ramcharan <info@prashant-ramcharan.co.uk>
2018-02-23 22:33:01 +0000

    Enhancement: Adds option to specify non standard VM options.

---
+++ src/main/java/courgette/runtime/CourgetteSystemProperty.java
@@ -0,0 +1,9 @@
+package courgette.runtime;
+
+final class CourgetteSystemProperty {
+    final static String THREADS = "courgette.threads";
+    final static String RUN_LEVEL = "courgette.runLevel";
+    final static String RERUN_FAILED_SCENARIOS = "courgette.rerunFailedScenarios";
+    final static String SHOW_TEST_OUTPUT = "courgette.showTestOutput";
+    final static String VM_OPTIONS = "courgette.vmoptions";
+}
