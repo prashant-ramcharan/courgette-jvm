6dab9b6e3e85c5c685a51fdbf6161c1a621c8482
Andrejs Cunskis <acunskis@evolutiongaming.com>
2018-04-10 16:51:04 +0300

    Enhanced handling of string system properties

--- src/main/java/courgette/runtime/CourgetteRunOptions.java
+++ src/main/java/courgette/runtime/CourgetteRunOptions.java
@@ -37,7 +37,7 @@ public class CourgetteRunOptions implements CourgetteOptions {
 
     @Override
     public String reportTargetDir() {
-        return System.getProperty(CourgetteSystemProperty.REPORT_TARGET_DIR, courgetteOptions.reportTargetDir());
+        return SystemPropertyUtils.getStringProperty(CourgetteSystemProperty.REPORT_TARGET_DIR, courgetteOptions.reportTargetDir());
     }
 
     @Override
