757401592c28afee281c36f0df3f01f93d023bad
Prashant Ramcharan <github@prashant-ramcharan.co.uk>
2020-11-21 23:46:37 +0000

    Revert FileUtils

--- src/main/java/courgette/runtime/utils/FileUtils.java
+++ src/main/java/courgette/runtime/utils/FileUtils.java
@@ -2,43 +2,41 @@ package courgette.runtime.utils;
 
 import courgette.runtime.CourgetteException;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import static courgette.runtime.CourgetteException.printExceptionStackTrace;
 
 public final class FileUtils {
 
     public static String readFile(String file, Boolean deleteOnExit) {
-        try {
-            File fileToRead = new File(file);
+        return fileReader.apply(file, deleteOnExit);
+    }
 
-            if (deleteOnExit) {
-                fileToRead.deleteOnExit();
-            }
-
-            return Files.readString(Paths.get(file));
-
-        } catch (IOException e) {
-            printExceptionStackTrace(e);
-        }
-        return null;
+    public static void writeFile(String file, Collection<?> contents) {
+        fileCollectionWriter.accept(file, contents);
     }
 
     public static void writeFile(String file, String contents) {
-        try {
-            Files.writeString(Paths.get(file), contents);
-        } catch (IOException e) {
-            printExceptionStackTrace(e);
-        }
+        fileStringWriter.accept(file, contents);
+    }
+
+    public static List<File> getParentFiles(String path) {
+        return Arrays.asList(new File(path).getParentFile().listFiles());
     }
 
     public static File getClassPathFile(String path) {
@@ -88,4 +86,67 @@ public final class FileUtils {
             // no action is needed
         }
     }
+
+    private static BiFunction<String, Boolean, String> fileReader = (file, deleteOnExit) -> {
+        if (file == null) {
+            return null;
+        }
+
+        File rerunFile = new File(file);
+
+        if (deleteOnExit) {
+            rerunFile.deleteOnExit();
+        }
+
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(rerunFile);
+            bufferedReader = new BufferedReader(fileReader);
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (Exception ignored) {
+            return null;
+        } finally {
+            try {
+                if (fileReader != null)
+                    fileReader.close();
+
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (Exception ignored) {
+            }
+        }
+    };
+
+    private static BiConsumer<String, String> fileStringWriter = (file, source) -> {
+        if (file == null) {
+            return;
+        }
+
+        File newFile = new File(file);
+        try {
+            FileWriter fileWriter = new FileWriter(newFile, Boolean.FALSE);
+            fileWriter.write(source);
+            fileWriter.close();
+        } catch (Exception ignored) {
+        }
+    };
+
+    private static BiConsumer<String, Collection<?>> fileCollectionWriter = (file, source) -> {
+        if (file == null) {
+            return;
+        }
+
+        File newFile = new File(file);
+        try {
+            FileWriter fileWriter = new FileWriter(newFile, Boolean.FALSE);
+
+            final StringBuilder contents = new StringBuilder();
+            source.forEach(data -> contents.append(data).append("\n"));
+            fileWriter.write(contents.toString());
+
+            fileWriter.close();
+        } catch (Exception ignored) {
+        }
+    };
 }
